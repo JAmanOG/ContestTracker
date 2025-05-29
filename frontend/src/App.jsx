5750c050999838c3a3e8894205801a092b982152
AmanJaiswal <jaman0120@gmail.com>
2025-05-29 16:43:32 +0530

    feat: add @vercel/analytics for improved application analytics tracking

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import Layout from "./layout/layout";
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   return (
     <>
       <Layout />
+      <Analytics />
     </>
   );
 }
